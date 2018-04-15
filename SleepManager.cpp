2fa92615f9b25bb2659e1d27ec85b53809c92d97
JakeS0ft <JakeSoft@yahoo.com>
2018-04-15 15:43:47 -0400

    Fix license prologue in .cpp file

--- SleepManager.cpp
+++ SleepManager.cpp
@@ -18,9 +18,6 @@
  *
  *  Created on: Apr 13, 2018
  *      Author: JakeSoft
- *
- *  This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
- *  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
  */
 
 #include "Arduino.h"
